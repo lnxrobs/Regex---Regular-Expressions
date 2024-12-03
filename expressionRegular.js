ac894631a9ff43eb1cac830a77b0f9e7d704c7e8
Robson <146855243+lnxrobs@users.noreply.github.com>
2024-12-03 15:56:21 -0300

    Update expressionRegular.js

--- expressionRegular.js
+++ expressionRegular.js
@@ -79,10 +79,10 @@
 //Codigo antigo 
 ^[A-Z]{2,3}[0-9]{4}|[A-Z]{3,4}[0-9]{3}|[A-Z0-9]{7}
 
-^(?:[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}|[A-Z0-9]{7})$
 
-//Acima Tentativa de correção com o novo codigo 
+//Abaixo tentativa de correção com o novo codigo 
  
+^(?:[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}|[A-Z0-9]{7})$
 
 
 
